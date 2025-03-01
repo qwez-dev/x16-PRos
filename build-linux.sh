0fcf23a4ad61a1c694c1a4391ee32ffbe7f68378
PRoX2011 <168980588+PRoX2011@users.noreply.github.com>
2025-03-01 15:32:49 +0500

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,14 +1,14 @@
 echo "Compiling the bootloader"
-nasm -f bin boot.asm -o bin/boot.bin
+nasm -f bin boot.asm -o boot.bin
 
 echo "Compiling the kernel"
-nasm -f bin kernel.asm -o bin/kernel.bin
+nasm -f bin kernel.asm -o kernel.bin
 
 echo "Creating a disk image"
-dd if=/dev/zero of=disk_img/x16pros.img bs=512 count=16
+dd if=/dev/zero of=x16pros.img bs=512 count=16
 
-dd if=bin/boot.bin of=disk_img/x16pros.img conv=notrunc
-dd if=bin/kernel.bin of=disk_img/x16pros.img bs=512 seek=1 conv=notrunc
+dd if=boot.bin of=x16pros.img conv=notrunc
+dd if=kernel.bin of=x16pros.img bs=512 seek=1 conv=notrunc
 echo "Done."
 
 qemu-system-i386 -hda disk_img/x16pros.img
